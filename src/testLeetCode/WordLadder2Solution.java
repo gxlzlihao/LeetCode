a1d5cb2203e1499da500cffc73b89dbabff49034
gxlzlihao <gxlzlihao@gmail.com>
2015-04-12 22:34:10 -0400

    solved Word Ladder 2 problem

--- src/testLeetCode/WordLadder2Solution.java
+++ src/testLeetCode/WordLadder2Solution.java
@@ -3,81 +3,96 @@ package testLeetCode;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import java.util.Set;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class WordLadder2Solution {
 	
 	private List<List<String>> res = new ArrayList<List<String>>();
 	private Set<String> used = new HashSet<String>();
-	private String candidate = "qwertyuioplkjhgfdsazxcvbnm";
-	private Map<String, Set<String>> parents = new HashMap<String, Set<String>>();
+	private Map<String, Set<String>> parents = new LinkedHashMap<String, Set<String>>();
 	private int length = Integer.MAX_VALUE;
 	
 	public List<List<String>> findLadders(String start, String end, Set<String> dict) {
         
-        Queue<String> queue = new ConcurrentLinkedQueue<String>();
-        queue.add(end);
+		Set<String> currentLevel = new HashSet<String>();
+        currentLevel.add(end);
+        Set<String> previousLevel = new HashSet<String>();
+        previousLevel.addAll(currentLevel);
         boolean found = false;
-		while ( queue.size() > 0 && !found ) {
-			Queue<String> newQueue = new ConcurrentLinkedQueue<String>();
-			while ( queue.size() > 0 ) {
-				String str = queue.poll();
+        length = 1;
+		while ( currentLevel.size() > 0 && !found ) {
+			Set<String> nextLevel = new HashSet<String>();
+			Iterator<String> iter = currentLevel.iterator();
+			while ( iter.hasNext() ) {
+				String str = iter.next();
 				used.add(str);
-				for ( int target = 0; target < str.length(); ++target )
-					for ( int rep = 0; rep < candidate.length(); ++rep )
-						if ( str.charAt(target) != candidate.charAt(rep) ) {
-							String newStr = str.substring(0, target) + candidate.substring(rep, rep+1) + str.substring(target+1);
-							if ( newStr.equals(start) ) {
-								found = true;
-								if ( parents.get(newStr) == null ) {
-									Set<String> fathers = new HashSet<String>();
-									fathers.add(str);
-									parents.put(newStr, fathers);
-								} else {
-									parents.get(newStr).add(str);
-								}
-							} else if ( dict.contains(newStr) && !used.contains(newStr) ) {
-								newQueue.add(newStr);
-								if ( parents.get(newStr) == null ) {
-									Set<String> fathers = new HashSet<String>();
-									fathers.add(str);
-									parents.put(newStr, fathers);
-								} else {
-									parents.get(newStr).add(str);
-								}
+				for ( String newStr : neighbours( str, dict ) ) {
+						if ( newStr.equals(start) ) {
+							found = true;
+							if ( parents.get(newStr) == null ) {
+								Set<String> fathers = new HashSet<String>();
+								fathers.add(str);
+								parents.put(newStr, fathers);
+							} else {
+								parents.get(newStr).add(str);
+							}
+							
+						} else if ( !used.contains(newStr) && !previousLevel.contains(newStr) ) {
+							nextLevel.add(newStr);
+							if ( parents.get(newStr) == null ) {
+								Set<String> fathers = new HashSet<String>();
+								fathers.add(str);
+								parents.put(newStr, fathers);
+							} else {
+								parents.get(newStr).add(str);
 							}
 						}
-				queue = newQueue;
+					}
 			}
+			previousLevel = currentLevel;
+			currentLevel = nextLevel;
+			++length;
 		}
-		
 		List<String> path = new ArrayList<String>();
 		path.add(start);
-		dfs( start, end, path, parents );
-		List<List<String>> finalRes = new ArrayList<List<String>>();
-		for ( List<String> solution : res ) 
-			if ( solution.size() == length )
-				finalRes.add(solution);
-		res = finalRes;
+		dfs( start, end, path, parents, 1 );
+		
+		parents.clear();
+		currentLevel.clear();
+		previousLevel.clear();
+		
         return res;
     }
 	
-	private void dfs( String str, String end, List<String> path, Map<String, Set<String>> parents ) {
-		if ( str.equals(end) ) {
-			if ( path.size() < length ) length = path.size();
-			List<String> newPath = new ArrayList<String>();
-			newPath.addAll(path);
-			res.add(newPath);
-		} else if ( parents.get(str) != null ) {
-			Set<String> fathers = parents.get(str);
-			for ( String father : fathers ) {
-				path.add( father );
-				dfs( father, end, path, parents );
-				path.remove( path.size() - 1 );
+	private List<String> neighbours ( String s, Set<String> dict ) {
+		List<String> res = new ArrayList<String>();
+		char[] chars = s.toCharArray();
+		for ( int target = 0; target < s.length(); ++target ) {
+			char original = chars[target];
+			for ( char rep = 'a'; rep <= 'z'; ++rep ) 
+				if ( original != rep ) {
+					chars[target] = rep;
+					String newStr = new String( chars );
+					if ( dict.contains(newStr) ) res.add(newStr);
+				}
+			chars[target] = original;
+		}
+		return res;
+	}
+	
+	private void dfs( String str, String end, List<String> path, Map<String, Set<String>> parents, int depth ) {
+		if ( depth > length ) return;
+		else if ( str.equals(end) && depth <= length ) {
+			res.add( new ArrayList<String>(path) );
+		} else if ( depth < length && parents.get(str) != null ) {
+			for ( String father : parents.get(str) ) {
+					path.add( father );
+					dfs( father, end, path, parents, depth + 1 );
+					path.remove( path.size() - 1 );
 			}
 		} else return;
 	}
